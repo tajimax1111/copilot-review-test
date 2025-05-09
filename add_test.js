1f09c523e47dd37f8e6f80c7adeaf713bcb2c845
tajimax1111 <y.tajima@hybrid-technologies.co.jp>
2025-05-09 15:32:31 +0900

    Update add_test.js

--- add_test.js
+++ add_test.js
@@ -1,3 +1,14 @@
-function sayHello() {
-  console.log("Hello World");
+// 非効率な関数：余計な変数と複雑なロジックを使っている
+function getEvenNumbers(arr) {
+  let result = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      let isEven = true;
+      if (!isEven) {
+        continue;
+      }
+      result.push(arr[i]);
+    }
+  }
+  return result;
 }
